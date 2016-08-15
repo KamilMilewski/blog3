4656bc7b966e353ee4c7a9b34441c9b2b01298b4
Kamil <kamil.milewski@hotmail.com>
2016-08-15 10:53:44 +0200

    Setup contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'contacts/controller'
+
   root 'welcome#index'
   get 'welcome/index'
  
   resources :posts
   resources :projects
+  resources :contacts, only: [:new, :create]
 end
