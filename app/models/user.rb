9285cf7616afc9be95056dece863e067f9fd4ec6
Kamil <kamil.milewski@hotmail.com>
2016-08-19 20:21:29 +0200

    remove git signup funcionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, #:registerable, -- because we dont want to allow users to register
          :recoverable, :rememberable, :trackable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   resources :posts
   resources :projects
   resources :contacts, only: [:new, :create]
+
+  get '*path' => redirect('/')
 end
